3fe6af3212cca6f63220a43e19cb4c4e724fbd5b
sadegh15khedry <sadegh15khedry@gmail.com>
2024-12-03 18:21:25 +0330

    minor conflict fix

--- src/classes/connection.py
+++ src/classes/connection.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Connection:
     def __init__(self, id, name, source, destination):
         self.id = id
@@ -38,47 +37,3 @@ class Connection:
         if self.destination:
             self.destination.set_level()
             
-
-        
-
-=======
-class Connection:
-    def __init__(self, id, name, source, destination):
-        self.id = id
-        self.name = name
-        self.source = source
-        self.destination = destination
-        self.current_value = 'U'
-        self.value_time = 0
-        self.history_of_values = []
-        self.history_of_times = []
-        self.history_id = 1
-        self.level = None
-        
-    def update_value(self, value, time):
-        # print(type(time))
-        
-        # time = int(time)
-        self.current_value = value
-        self.value_time = time
-        self.history_of_values.append(self.current_value)
-        self.history_of_times.append(self.value_time)
-        
-        # print(f"updated conneciton:{self.name}, to value:{self.current_value}")
-        self.history_id += 1
-
-            
-    def print_output(self, time):
-        print(f"output connection: {self.name}  value: {self.current_value}")
-        
-        
-    def set_level(self, value):
-        self.level = value
-        # print (f"circuit input: {self.name} level: {self.level}") 
-        if self.destination:
-            self.destination.set_level()
-            
-
-        
-
->>>>>>> 3c475944df4aba9bda4a2460c486b542af8f69a0
--- src/scripts/simulation.py
+++ src/scripts/simulation.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from iscas_parser import parse_iscas
 from io_file_work import load_csv_file, generate_input_file
 
@@ -9,47 +9,6 @@ def simulation(circuit_path, inputs_path, test_vectors_path, delay_consideration
     circuit.set_levels()
     circuit.set_controlability()
     circuit.set_observability()
-    circuit.draw_circuit()
-    
-    
-    input_file = load_csv_file(inputs_path)
-    
-    
-    if(input_file is None and input_file_generation == True):
-        generate_input_file(circuit, inputs_path)
-        input_file = load_csv_file(inputs_path)
-
-    circuit.check_delays() 
-    time = 0
-    while time < max_iterations:
-
-
-        time = int(time)
-
-                
-        print(f"Time step: {time} started -----------------------------")
-    
-        # time_step_inputs = input_file[input_file['time'] == time]
-        time_step_inputs = input_file[input_file['time'] == time]
-        circuit.set_circuit_inputs(time_step_inputs, time)
-        circuit.pass_values_to_output(time, delay_consideration)
-        circuit.print_connections_values()
-        
-        print(f"Time step: {time} ended -----------------------------")
-        time = time + 1
-
-
-
-
-=======
-from iscas_parser import parse_iscas
-from io_file_work import load_csv_file, generate_input_file
-
-def simulation(circuit_path, inputs_path, test_vectors_path, delay_consideration, max_iterations, input_file_generation):
-    
-    circuit = parse_iscas(circuit_path)
-    circuit.initialize_net_connections()
-    circuit.set_levels()
     circuit.draw_circuit()
     
     
@@ -78,8 +37,3 @@ def simulation(circuit_path, inputs_path, test_vectors_path, delay_consideration
         
         print(f"Time step: {time} ended -----------------------------")
         time = time + 1
-
-
-
-
->>>>>>> 3c475944df4aba9bda4a2460c486b542af8f69a0
